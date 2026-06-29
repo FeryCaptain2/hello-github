0a4593ed9aa40d4dbc9dbc6fd96f2d434671ce18
Farhad <97361013+FeryCaptain2@users.noreply.github.com>
2026-06-30 01:01:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def main():
     
     name = input("اسمت چی چیه؟ ")
     print(f"\nخوش آمدی {name}!")
-    print("امیدوارم GitHub ت همیشه فعال باشه 🚀\n")
+    print("امیدوارم GitHub ت همیشه فعال باشد 🚀\n")
     
     print("چند مثال ساده:")
     print(f"2 + 2 = {2 + 2}")
     print(f"5 * 3 = {5 * 3}")
     print(f"10 / 2 = {10 / 2}")
     
-    print("\nموفق باشی!")
+    print("\nموفق باشید!")
 
 if __name__ == "__main__":
     main()
