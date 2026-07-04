c829e159bf63311c1d0f31a6f34b04102b52120c
Farhad <97361013+FeryCaptain2@users.noreply.github.com>
2026-07-04 15:52:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("سلام جهان! 👋")
-    print("این اولین .....zzzzzzzzzzz..00.0zzzz000z.000000........پروژه من تو GitHub هست.\n")
+    print("این اولین .....zzzzzzzzzzz..00.0zzzz000z.0000000........پروژه من تو GitHub هست.\n")
     
     name = input("اسمت رو بنویس؟ ")
     print(f"\nخوش آمدی {name}!")
